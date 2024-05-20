751799c5fd270f741914ae68fd5a698eb4e93621
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2024-05-20 14:42:42 +0200

    Initial commit

---
+++ Common/src/main/java/com/christofmeg/brutalharvest/CommonConstants.java
@@ -0,0 +1,7 @@
+package com.christofmeg.brutalharvest;
+
+public class CommonConstants {
+
+    public static final String MODID = "brutalharvest";
+
+}
---
+++ NeoForge/src/main/java/com/christofmeg/brutalharvest/BrutalHarvest.java
@@ -0,0 +1,13 @@
+package com.christofmeg.brutalharvest;
+
+import net.minecraftforge.fml.common.Mod;
+
+@SuppressWarnings("unused")
+@Mod(CommonConstants.MODID)
+public class BrutalHarvest {
+
+    public BrutalHarvest() {
+
+    }
+
+}
