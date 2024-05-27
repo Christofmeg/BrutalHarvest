432346a68f2e60064de616a11424fe767c2e1faf
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2024-05-27 06:47:47 +0200

    Update BlockRegistry.java

--- Forge/src/main/java/com/christofmeg/brutalharvest/common/init/BlockRegistry.java
+++ Forge/src/main/java/com/christofmeg/brutalharvest/common/init/BlockRegistry.java
@@ -1,10 +1,15 @@
 package com.christofmeg.brutalharvest.common.init;
 
 import com.christofmeg.brutalharvest.CommonConstants;
+import com.christofmeg.brutalharvest.common.block.CropSupportBlock;
+import com.christofmeg.brutalharvest.common.block.TomatoCropBlock;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,6 +22,8 @@ public class BlockRegistry {
 
     public static final DeferredRegister<Block> BLOCKS;
 
+//    public static final RegistryObject<Block> CROP_SUPPORT;
+    public static final RegistryObject<Block> TOMATO_PLANT;
 
     private BlockRegistry() {
     }
@@ -27,10 +34,16 @@ public class BlockRegistry {
 
     static {
         BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, CommonConstants.MOD_ID);
+
+//        CROP_SUPPORT = BLOCKS.register("crop_support", () -> new CropSupportBlock(BlockBehaviour.Properties.of().noCollission().instabreak().sound(SoundType.BAMBOO_WOOD).pushReaction(PushReaction.DESTROY)));
+//        ItemRegistry.ITEMS.register("crop_support", () -> new BlockItem(CROP_SUPPORT.get(), new Item.Properties()));
+
+        TOMATO_PLANT = BLOCKS.register("tomato_plant", () -> new TomatoCropBlock(BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).noCollission().randomTicks().instabreak().sound(SoundType.CROP).pushReaction(PushReaction.DESTROY)));
+
     }
 
     private static RegistryObject<Block> block(String name, BlockBehaviour.Properties blockProperties) {
-        RegistryObject<Block> block = BlockRegistry.BLOCKS.register(name, () -> new Block(blockProperties));
+        RegistryObject<Block> block = BLOCKS.register(name, () -> new Block(blockProperties));
         ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
         return block;
     }
