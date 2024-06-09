67a8d3449d1ad54c06b2f648a424e77e8c4f1b66
Tristan hache <tristan@tristanapp.com>
2024-06-09 18:20:41 -0400

    Added all scythes

--- Forge/src/main/java/com/christofmeg/brutalharvest/common/data/BrutalAdvancementProvider.java
+++ Forge/src/main/java/com/christofmeg/brutalharvest/common/data/BrutalAdvancementProvider.java
@@ -7,6 +7,7 @@ import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.FrameType;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.advancements.critereon.RecipeCraftedTrigger;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.network.chat.Component;
@@ -36,7 +37,7 @@ public class BrutalAdvancementProvider extends ForgeAdvancementProvider {
                             Component.translatable(CommonConstants.MOD_NAME),
                             Component.translatable(CommonConstants.MOD_ID + "." + "advancement.root.desc"),
                             new ResourceLocation("minecraft:textures/block/rooted_dirt.png"),
-                    FrameType.TASK, true, true, false)
+                            FrameType.TASK, true, true, false)
                     .addCriterion("0", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.TOMATO.get()))
                     .save(consumer, getNameId("root"));
 
@@ -50,6 +51,15 @@ public class BrutalAdvancementProvider extends ForgeAdvancementProvider {
                     .addCriterion("0", TomatoProjectileTrigger.TriggerInstance.simple())
                     .save(consumer, getNameId("rotten_tomatoes"));
 
+            Advancement.Builder.advancement()
+                    .display(ItemRegistry.STONE_SCYTHE.get(),
+                            Component.translatable(CommonConstants.MOD_ID + "." + "advancement.grim_reaper"),
+                            Component.translatable(CommonConstants.MOD_ID + "." + "advancement.grim_reaper.desc"),
+                            null,
+                            FrameType.TASK, true, true, false)
+                    .parent(root)
+                    .addCriterion("0", RecipeCraftedTrigger.TriggerInstance.craftedItem(ItemRegistry.STONE_SCYTHE.getId()))
+                    .save(consumer, getNameId("grim_reaper"));
         }
 
         private String getNameId(String id) {
