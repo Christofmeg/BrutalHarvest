9ead793ba487283d4ee66b36e6962e8d5a4aaaec
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2024-07-16 12:55:57 +0200

    On-hit mob effects to rotten tomatoes

--- Forge/changelog.md
+++ Forge/changelog.md
@@ -1,13 +1,2 @@
-1.6.0:
-* Added colored fabrics
-* Updated Tomato Slice texture
-* Updated Onion Seeds texture
-* Updated Seed Satchel texture
-* Updated Coffee Seeds texture
-* Updated Tomato Seeds texture
-* Added textures for future items
-* Tweaked fabric recipe
-* Moved the throwing of tomatoes to sneak right click
-* Fixed food nutrition and saturation formula
-* Fixed food mob effects not working properly
-* Bump modloader version
+1.6.1:
+* Added on-hit mob effects to rotten tomatoes
--- Forge/src/main/java/com/christofmeg/brutalharvest/common/entity/TomatoProjectileEntity.java
+++ Forge/src/main/java/com/christofmeg/brutalharvest/common/entity/TomatoProjectileEntity.java
@@ -2,11 +2,14 @@ package com.christofmeg.brutalharvest.common.entity;
 
 import com.christofmeg.brutalharvest.common.init.AdvancementRegistry;
 import com.christofmeg.brutalharvest.common.init.EntityTypeRegistry;
+import com.christofmeg.brutalharvest.common.init.ItemRegistry;
 import com.christofmeg.brutalharvest.common.init.SoundRegistry;
 import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -19,6 +22,8 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
 
 public class TomatoProjectileEntity extends ThrowableItemProjectile {
 
@@ -62,8 +67,19 @@ public class TomatoProjectileEntity extends ThrowableItemProjectile {
                     AdvancementRegistry.ROTTEN_TOMATOES.trigger(serverPlayer);
                 }
             }
+            Random random = new Random();
+            int randomInt = random.nextInt(10);
+           if (randomInt == 0) {
+                if (this.getItem().getItem() == ItemRegistry.ROTTEN_TOMATO.get()) {
+                    if (entity instanceof LivingEntity livingEntity) {
+                        MobEffectInstance poison = new MobEffectInstance(MobEffects.POISON, 40);
+                        MobEffectInstance blindness = new MobEffectInstance(MobEffects.BLINDNESS, 40);
+                        livingEntity.addEffect(poison);
+                        livingEntity.addEffect(blindness);
+                    }
+                }
+           }
         }
-
     }
 
     protected void onHit(@NotNull HitResult hitResult) {
