352890649f0922d127a570dd4cabb30fb64b637d
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2024-07-20 19:11:15 +0200

    Tweak tomato seed from Tomato Slice

--- Forge/src/main/java/com/christofmeg/brutalharvest/common/data/BrutalRecipeProvider.java
+++ Forge/src/main/java/com/christofmeg/brutalharvest/common/data/BrutalRecipeProvider.java
@@ -191,7 +191,7 @@ public class BrutalRecipeProvider extends BaseRecipeProvider {
                 .unlockedBy("tomato_seeds", has(ItemRegistry.TOMATO_SEEDS.get()))
                 .save(consumer, modLoc("tomato_seeds_from_rotten_tomato"));
 
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ItemRegistry.TOMATO_SEEDS.get(), 2)
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ItemRegistry.TOMATO_SEEDS.get())
                 .requires(ItemRegistry.TOMATO_SLICE.get())
                 .unlockedBy("tomato_seeds", has(ItemRegistry.TOMATO_SEEDS.get()))
                 .save(consumer, modLoc("tomato_seeds_from_tomato_slice"));
